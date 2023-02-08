a958a6f3498814e6f6f5f55ee36461408dd65db6
Lautaro Muiño <117776336+LautaroMuino03@users.noreply.github.com>
2023-02-08 12:26:31 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,25 @@
+//listas
+var a = 19;
+console.log(a);
+
+
+const constante = "Lautaro Muiño";
+console.log(constante)
+
+var boolean = "¿eres desarrollador?"
+boolean = "false";
+console.log(boolean)
+
+const fecha_cadena = new Date("february 23 2003")
+console.log(fecha_cadena);
+
+const libro = {
+    titulo: ["El código Da Vinci"],
+    autor: ["Brown, Dan"],
+    mifecha: new Date ("March 18 2003"),
+    miurl: new URL ("https://www.libooks.com/libros/el-codigo-da-vinci-978-987-580-885-0"), 
+} 
+
+console.log(libro); 
+
+const lista = [19, "Lautaro Muiño", false, "March 18 2003", "El Código Da Vinci"]
